588d6f62e1d2df8218aaee916684567d0e02a9e7
Aung Thu Myint <63327282+AungThuMyint@users.noreply.github.com>
2021-01-29 21:50:46 +0630

    Update XFCE.sh

--- XFCE/XFCE.sh
+++ XFCE/XFCE.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-#Get the necessary components
 apt-mark hold udisks2
-[ ! -f /root/.parrot ] && apt-get update || echo "Parrot detected, not updating apt cache since that will break the whole distro"
+[ ! -f /root/.parrot ] && apt-get update
 apt-get install keyboard-configuration -y
 apt-get install sudo wget -y
 apt-get install xfce4 xfce4-terminal tigervnc-standalone-server -y
 apt-get install xfe -y
 apt-get clean
 
-#Setup the necessary files
 mkdir ~/.vnc
-wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/XFCE4/xstartup -P ~/.vnc/
-wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/XFCE4/vncserver-start -P /usr/local/bin/
-wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/XFCE4/vncserver-stop -P /usr/local/bin/
+wget https://raw.githubusercontent.com/AungThuMyint/TermuxLinuxFiles/main/XFCE/xstartup -P ~/.vnc/
+wget https://raw.githubusercontent.com/AungThuMyint/TermuxLinuxFiles/main/XFCE/vncserver-start -P /usr/local/bin/
+wget https://raw.githubusercontent.com/AungThuMyint/TermuxLinuxFiles/main/XFCE/vncserver-stop -P /usr/local/bin/
 
 chmod +x ~/.vnc/xstartup
 chmod +x /usr/local/bin/vncserver-start
@@ -22,8 +20,8 @@ chmod +x /usr/local/bin/vncserver-stop
 echo " "
 
 echo "Running browser patch"
-wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Uninstall/ubchromiumfix.sh && chmod +x ubchromiumfix.sh
-./ubchromiumfix.sh && rm -rf ubchromiumfix.sh
+wget https://raw.githubusercontent.com/AungThuMyint/TermuxLinuxFiles/main/XFCE/Fix.sh && chmod +x Fix.sh
+./Fix.sh && rm -rf Fix.sh
 
 echo "You can now start vncserver by running vncserver-start"
 echo " "
